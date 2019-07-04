0af0ad8c094a545be6da6abc73fbe485673ce14b
Jeremy <jeremyzelaya@hotmail.es>
2019-07-04 00:49:25 -0600

    Created Capa de Datos
    Created the main config class and methods.

---
+++ capa_presentacion/Capa de Presentacion/Clases/configuracion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capa_de_Presentacion
+{
+    class configuracion
+    {
+        
+        // Class for returning the connection string for the datebase
+        public static string connection_string = "Data Source=.;Initial Catalog=Victus;Integrated Security=True";
+
+        public string GetConnectionString() {
+            return connection_string;
+        }
+    }
+}
---
+++ capa_presentacion/Capa de Presentacion/Clases/metodo_datos.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Capa_de_Presentacion
+{
+    class metodo_datos
+    {
+        // Get SQLCommand
+        public SqlCommand GetCommand() {
+            configuracion config = new configuracion();
+
+            string ConnectionString = config.GetConnectionString();
+            SqlConnection SQLConnection = new SqlConnection();
+            SqlCommand Command = new SqlCommand();
+
+            SQLConnection.ConnectionString = ConnectionString;
+            Command = SQLConnection.CreateCommand();
+            Command.CommandType = System.Data.CommandType.Text;
+
+            return Command;
+        }
+        // Exec SQLCommand
+        public int ExecCommand(SqlCommand command) {
+            int i;
+            try
+            {
+                command.Connection.Open();
+                i = command.ExecuteNonQuery();
+                return i;
+            }
+            catch (Exception)
+            {
+                return -1;
+                throw;
+            }
+            finally {
+                command.Connection.Close();
+            }
+        }
+        // Exec Query
+        public DataTable ExecSearch(SqlCommand command) {
+            DataTable table = new DataTable();
+            SqlDataAdapter Adapter = new SqlDataAdapter();
+            try
+            {
+                command.Connection.Open();
+                Adapter.SelectCommand = command;
+                Adapter.Fill(table);
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally{
+                command.Connection.Close();
+            }
+            return table;
+        }
+
+    }
+}
